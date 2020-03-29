42f3cd655eb28a106e321e902c2701c365ab68ab
Neil Fulwiler <neil.fulwiler@gmail.com>
2020-03-29 15:10:35 +0100

    include new secret file

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -14,13 +14,13 @@ security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./cordova-app/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./cordova-app/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./cordova-app/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./cordova-app/ios_development.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#security import ./cordova-app/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#security import ./cordova-app/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 # see https://docs.travis-ci.com/user/common-build-problems/
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./cordova-app/Geovation.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "./cordova-app/PlasticPatrol_Dev.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
