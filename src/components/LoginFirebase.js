b6115402b91767fe78c9d8c95134fa80d3c094f9
Neil Fulwiler <neil.fulwiler@gmail.com>
2020-10-03 12:10:19 +0100

    check if its android and dont show apple login

--- src/components/LoginFirebase.js
+++ src/components/LoginFirebase.js
@@ -13,17 +13,19 @@ import { authFirebase } from "features/firebase";
 
 // TODO: change theme: https://github.com/firebase/firebaseui-web-react/tree/master/dist
 
+const devicePlatform = window.device && window.device.platform;
+
 const LoginFirebase = (props) => {
   const { open, handleClose, onSignIn } = props;
 
   const uiConfig = {
     signInSuccessUrl: "/",
     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-    signInOptions: [
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      "apple.com"
-    ],
+    signInOptions:
+      [
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ] + [devicePlatform === "Android" ? [] : "apple.com"],
     callbacks: {
       // Avoid redirects after sign-in.
       signInSuccessWithAuthResult: (result) => {
